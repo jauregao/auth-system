da90e44cccb456e80c786261850aba0cca570600
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-03-21 13:20:02 -0300

    setup inicial

---
+++ src/database/dump.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(80)  UNIQUE NOT NULL,
+  pass VARCHAR(50)  NOT NULL,
+  full_name VARCHAR(80) NOT NULL,
+  registration_date DATE NOT NULL
+);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export type User = {
+  id: number
+  full_name: string
+  email: string
+  pass: string
+  registration_date: Date
+}
